09e393e32f3d8a310c8d48e7ccba22b12ca2f1d2
ionutbila <ionutbila@ymail.com>
2024-01-22 15:56:17 +0200

    Curs 6

---
+++ src/Lamp.java
@@ -0,0 +1,34 @@
+public class Lamp {
+    private boolean lightState;
+    private String color = "red";
+
+    private int intensity;
+
+    private boolean addedLater;
+
+    Lamp() {
+        System.out.println("Starting construction of lamp");
+        lightState = false;
+        intensity = 5;
+    }
+
+    Lamp(String color, int intensity){
+        System.out.println("Starting construction of lamp with color " + color + " and intensity " + intensity);
+        this.color = color;
+        this.intensity = intensity;
+    }
+
+    public void turnOn(){
+        System.out.println("Turning on with color " + color + " with intensity " + intensity);
+        lightState = true;
+    }
+
+    public void turnOff(){
+        System.out.println("Turning off");
+        lightState = false;
+    }
+
+    public void increaseIntensity(){
+        this.intensity++;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
-
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("This is an experminet!");
+        System.out.println("This is an experiment!");
         System.out.println("Hello world!");
+        Lamp lamp1 = new Lamp(); //Instantierea este egal fabricarii
+        Lamp lamp2 = new Lamp();
+
+        lamp1.turnOn();
+        lamp2.turnOn();
+
+        Lamp customLamp = new Lamp("green", 10);
+        customLamp.turnOn();
+        customLamp.increaseIntensity();
+        customLamp.turnOff();
+        customLamp.turnOn();
+
+        Random random = new Random();
+        random.nextInt();
+
+
+
 
     }
 }
