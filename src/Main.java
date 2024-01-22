fb183ebceb1d4f86007c4d1738c2c8387b6eeb33
ionutbila <ionutbila@ymail.com>
2024-01-22 16:34:46 +0200

    added some code for facebook login

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         System.out.println("This is an experminet!");
         System.out.println("Hello world!");
+        System.out.println("Working on login with facebook");
 
     }
 }
